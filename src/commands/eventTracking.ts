186f10ff721984222d637ff3a52711c4e3e5e75d
陳思瑜 Judy <laplus.space@gmail.com>
2025-08-07 19:36:46 +0800

    feat(src/commands): 新增 event tracking 指令

---
+++ src/commands/eventTracking.ts
@@ -0,0 +1,63 @@
+import { ClassConverter } from '@/converters';
+import { EventTracking } from '@/repositories';
+import { CommandType } from '@/types';
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { table } from 'table';
+
+export const eventQuery: CommandType = {
+    type: "eventTracking",
+    data: new SlashCommandBuilder()
+        .setName('event-tracking-query')
+        .setDescription('Query raw data by range')
+        .addStringOption(option =>
+            option
+                .setName('start')
+                .setDescription('A1 Notation')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName('end')
+                .setDescription('A1 Notation')
+                .setRequired(true)
+        ),
+    execute: async (interaction: ChatInputCommandInteraction) => {
+        const start = interaction.options.getString('start', true);
+        const end = interaction.options.getString('end', true);
+
+        const eventTracking = await EventTracking.getInstance();
+
+        const data = await eventTracking.get(start, end);
+
+        if (data.length === 0) {
+            await interaction.reply({ content: 'No data found for the specified range.' });
+            return
+        }
+
+        const users = interaction.client.users.cache;
+        const channels = interaction.client.channels.cache;
+
+        const header = Object.keys(data[0]);
+        const rows = data.map(i => {
+            const user = users.get(i.userId);
+            i.userId = user?.username ?? i.userId;
+
+            const channel = channels.get(i.channelId) as any;
+            i.channelId = channel?.name ?? i.channelId;
+            
+            const data = ClassConverter.serialize(i)
+            return Object.values(data)
+        })
+
+        const content = table([header, ...rows], {
+            header: {
+                alignment: 'center',
+                content: 'Event Tracking',
+            },
+            columnDefault: {
+                width: 15,
+            },
+        })
+        await interaction.reply({ content: '```' + content + '```' });
+    },
+};
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,2 +1,3 @@
+export * from "./eventTracking";
 export * from "./ping";
 
