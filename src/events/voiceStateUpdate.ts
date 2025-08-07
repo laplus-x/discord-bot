c576b7263ed0b04b072d001b0d08e5af4ec3d00d
陳思瑜 Judy <laplus.space@gmail.com>
2025-08-07 19:37:48 +0800

    feat(src/events): VoiceStateUpdate - 將事件紀錄到 google sheet

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,58 +1,108 @@
-import { EventType } from '@/types';
+import { EventTracking } from '@/repositories';
+import { ClientEventType, EventTrackingResource, EventType } from '@/types';
+import { Functions } from '@/utilities';
 import { ClientEvents, Events, VoiceState } from 'discord.js';
+import { nanoid } from 'nanoid';
 import EventEmitter from 'stream';
 import { match } from 'ts-pattern';
 
-const EventName = {
-    EnterVoiceChannel: "EnterVoiceChannel",
-    LeaveVoiceChannel: "LeaveVoiceChannel",
-    ChangeVoiceChannel: "ChangeVoiceChannel",
-    StartScreenSharing: "StartScreenSharing",
-    CloseScreenSharing: "CloseScreenSharing",
-} as const
-
 const events = new EventEmitter()
 
-events.on(EventName.EnterVoiceChannel, (oldState: VoiceState, newState: VoiceState) => {
+events.on(EventType.EnterVoiceChannel, async (oldState: VoiceState, newState: VoiceState) => {
     const member = newState.member;
-    console.log(`${member.user.tag} enters voice channel #${newState.channelId}`);
+    console.log(`${member?.user.tag} enters voice channel #${newState.channelId}`);
+    
+    const event = Object.assign(new EventTrackingResource(), {
+        id: nanoid(),
+        type: EventType.EnterVoiceChannel,
+        channelId: newState.channelId || '',
+        userId: member?.user.id ?? '',
+        timestamp: new Date()
+    })
+
+    const tracking = await EventTracking.getInstance();
+    const { ok, val } = await Functions.wrapAsync<void, Error>(() => tracking.append(event));
+    if (!ok) {
+        console.error(`Failed to track event: ${val.message}`);
+    }
 })
 
-events.on(EventName.LeaveVoiceChannel, (oldState: VoiceState, newState: VoiceState) => {
+events.on(EventType.LeaveVoiceChannel, async (oldState: VoiceState, newState: VoiceState) => {
     const member = newState.member;
-    console.log(`${member.user.tag} leaves voice channel #${oldState.channelId}`);
+    console.log(`${member?.user.tag} leaves voice channel #${oldState.channelId}`);
+    
+    const event = Object.assign(new EventTrackingResource(), {
+        id: nanoid(),
+        type: EventType.LeaveVoiceChannel,
+        channelId: oldState.channelId || '',
+        userId: member?.user.id ?? '',
+        timestamp: new Date()
+    })
+
+    const tracking = await EventTracking.getInstance();
+    const { ok, val } = await Functions.wrapAsync<void, Error>(() => tracking.append(event));
+    if (!ok) {
+        console.error(`Failed to track event: ${val.message}`);
+    }
 })
 
-events.on(EventName.ChangeVoiceChannel, (oldState: VoiceState, newState: VoiceState) => {
-    const member = newState.member;
-    console.log(`${member.user.tag} leaves voice channel #${oldState.channelId}`);
-    console.log(`${member.user.tag} enters voice channel #${newState.channelId}`);
+events.on(EventType.ChangeVoiceChannel, async (oldState: VoiceState, newState: VoiceState) => {
+    events.emit(EventType.LeaveVoiceChannel, oldState, newState);
+    events.emit(EventType.EnterVoiceChannel, oldState, newState);
 })
 
-events.on(EventName.StartScreenSharing, (oldState: VoiceState, newState: VoiceState) => {
+events.on(EventType.StartScreenSharing, async (oldState: VoiceState, newState: VoiceState) => {
     const member = newState.member;
-    console.log(`${member.user.tag} starts screen sharing`);
+    console.log(`${member?.user.tag} starts screen sharing`);
+    
+    const event = Object.assign(new EventTrackingResource(), {
+        id: nanoid(),
+        type: EventType.StartScreenSharing,
+        channelId: oldState.channelId || newState.channelId || '',
+        userId: member?.user.id ?? '',
+        timestamp: new Date()
+    })
+
+    const tracking = await EventTracking.getInstance();
+    const { ok, val } = await Functions.wrapAsync<void, Error>(() => tracking.append(event));
+    if (!ok) {
+        console.error(`Failed to track event: ${val.message}`);
+    }
 })
 
-events.on(EventName.CloseScreenSharing, (oldState: VoiceState, newState: VoiceState) => {
+events.on(EventType.CloseScreenSharing, async (oldState: VoiceState, newState: VoiceState) => {
     const member = newState.member;
-    console.log(`${member.user.tag} closes screen sharing`);
+    console.log(`${member?.user.tag} closes screen sharing`);
+    
+    const event = Object.assign(new EventTrackingResource(), {
+        id: nanoid(),
+        type: EventType.CloseScreenSharing,
+        channelId: oldState.channelId || newState.channelId || '',
+        userId: member?.user.id ?? '',
+        timestamp: new Date()
+    })
+    
+    const tracking = await EventTracking.getInstance();
+    const { ok, val } = await Functions.wrapAsync<void, Error>(() => tracking.append(event));
+    if (!ok) {
+        console.error(`Failed to track event: ${val.message}`);
+    }
 })
 
-export const voiceStateUpdate: EventType = {
+export const voiceStateUpdate: ClientEventType = {
     name: Events.VoiceStateUpdate as keyof ClientEvents,
     once: false,
     execute: (oldState: VoiceState, newState: VoiceState) => {
         const channel = match(true)
-            .when(() => oldState.channelId === null && newState.channelId !== null, () => EventName.EnterVoiceChannel)
-            .when(() => oldState.channelId !== null && newState.channelId === null, () => EventName.LeaveVoiceChannel)
-            .when(() => oldState.channelId !== newState.channelId, () => EventName.ChangeVoiceChannel)
+            .when(() => oldState.channelId === null && newState.channelId !== null, () => EventType.EnterVoiceChannel)
+            .when(() => oldState.channelId !== null && newState.channelId === null, () => EventType.LeaveVoiceChannel)
+            .when(() => oldState.channelId !== newState.channelId, () => EventType.ChangeVoiceChannel)
             .otherwise(() => null)
         if (channel) events.emit(channel, oldState, newState)
 
         const streaming = match(true)
-            .when(() => !oldState.streaming && newState.streaming, () => EventName.StartScreenSharing)
-            .when(() => oldState.streaming && !newState.streaming, () => EventName.CloseScreenSharing)
+            .when(() => !oldState.streaming && newState.streaming, () => EventType.StartScreenSharing)
+            .when(() => oldState.streaming && !newState.streaming, () => EventType.CloseScreenSharing)
             .otherwise(() => null)
         if (streaming) events.emit(streaming, oldState, newState)
     },
